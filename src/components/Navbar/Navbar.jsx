e3f41bfd739d2db292a121246a643e255b8c8b21
gyaltsentenzin <tenzingyaltsen15@gmail.com>
2023-11-26 21:53:40 -0600

    mostly done except updating project json file

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -36,7 +36,7 @@ export const Navbar = () => {
             <a href="#projects">Projects</a>
           </li>
           <li>
-            <a href="#contacts">Contact</a>
+            <a href="#contact">Contact</a>
           </li>
         </ul>
       </div>
